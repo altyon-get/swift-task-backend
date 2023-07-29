fbea4d8638dcee11b9bcf1c3bf2251bc12dfe4a8
altyon-get <73048959+altyon-get@users.noreply.github.com>
2023-07-30 00:48:43 +0530

    Update task.model.js
    description req:false

--- task.model.js
+++ task.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String },
     dueDate: { type: String, required: true },
     priority: { type: String, enum: ['low', 'medium', 'high'], required: true },
     status: { type: String, enum: ['to-do', 'in-progress', 'completed'], required: true },
